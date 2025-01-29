6eeedba9d60372848661767c1a3a0e7037b27a3b
naziya-usman <Naziyausmantwaiba@gmail.com>
2025-01-29 11:28:49 +0530

    delete method

--- server.js
+++ server.js
@@ -13,86 +13,73 @@ app.use(bodyParser.urlencoded({ extended: true }));
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
 
-  const EmployeeSchema = new mongoose.Schema({
-    name: String,
-    position: String,
-    skills: [String],
-    contact: {
-      email: String,
-      phone: String,
-    },
-    department: String,
-    experience: Number,
-  });
-
-  const Employee = mongoose.model("Employee", EmployeeSchema);
-  for (const empobj of data) {
-    const employeeData = new Employee(empobj);
-    employeeData.save().then((d) => {
-      console.log("saved");
+    const EmployeeSchema = new mongoose.Schema({
+        name: String,
+        position: String,
+        skills: [String],
+        contact: {
+            email: String,
+            phone: String,
+        },
+        department: String,
+        experience: Number,
     });
-  }
 
-  app.get("/employees", (req, res) => {
-    Employee.find({}).then((result) => {
-      res.json(result);
-    });
-  });
+    const Employee = mongoose.model("Employee", EmployeeSchema);
+    for (const empobj of data) {
+        const employeeData = new Employee(empobj);
+        employeeData.save().then((d) => {
+            console.log("saved");
+        });
+    }
 
-  app.get("/employees/:id", (req, res) => {
-    const employeedIdInRequest = req.params.id;
-    Employee.findById(employeedIdInRequest).then((result) => {
-      res.json(result);
+    app.get("/employees", (req, res) => {
+        Employee.find({}).then((result) => {
+            res.json(result);
+        });
     });
-  });
 
-  app.post("/employees", (req, res) => {
-    const newEmployee = req.body;
-    console.log(newEmployee);
-    const employeeData = new Employee(newEmployee);
-    employeeData.save().then((newData) => {
-      console.log(newData);
-      res.json(newData);
+    app.get("/employees/:id", (req, res) => {
+        const employeedIdInRequest = req.params.id;
+        Employee.findById(employeedIdInRequest).then((result) => {
+            res.json(result);
+        });
     });
-  });
-  // ////////////////////////
-  app.put("/employees/:id", (req, res) => {
-    const employeedIdInRequest = req.params.id;
-    const update = req.body;
-    Employee.findByIdAndUpdate(employeedIdInRequest, update, {
-      new: true,
-      runValidators: true,
-    }).then((data) => {
-      res.json(data);
-    });
-  });
 
-  app.delete("/employees/:id", (req, res) => {
-    const employeedIdInRequest = req.params.id;
-    Employee.findByIdAndDelete(employeedIdInRequest)
-      .then((result) => {
-        console.log("Document deleted:", result);
-        res.json("Document deleted:", result);
-      })
-      .catch((err) => {
-        console.error("Error:", err);
-        res.json(err);
-      });
-  });
+    app.post("/employees", (req, res) => {
+        const newEmployee = req.body;
+        console.log(newEmployee);
+        const employeeData = new Employee(newEmployee);
+        employeeData.save().then((newData) => {
+            console.log(newData);
+            res.json(newData);
+        });
+    });
+    // ////////////////////////
+    app.put("/employees/:id", (req, res) => {
+        const employeedIdInRequest = req.params.id;
+        const update = req.body;
+        Employee.findByIdAndUpdate(employeedIdInRequest, update, {}).then((data) => {
+            res.json(data);
+        });
+    });
+
+    app.delete("/employees/:id", (req, res) => {
+        const employeedIdInRequest = req.params.id;
+        Employee.findByIdAndDelete(employeedIdInRequest).then((result) => {
+            res.json("Document deleted: succussfuly deleted ");
+        });
+    });
 }
 
 function validation(emplyName) {
-  if (
-    !emplyName.name ||
-    typeof emplyName.name != "string" ||
-    emplyName.name.trim() == ""
-  ) {
-    throw Error("Name is required and must be a non-empty string");
-  }
+    if (!emplyName.name || typeof emplyName.name != "string" || emplyName.name.trim() == "") {
+        throw Error("Name is required and must be a non-empty string");
+    }
 }
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
